5bbd6fe5bfae48bf012e96f24fac2e7a8c4c72dd
Mark Tearle <mark.tearle@qcif.edu.au>
2021-08-13 15:30:21 +0800

    Improve example.py

--- example.py
+++ example.py
@@ -11,17 +11,27 @@ from download import make_logger
 logger = make_logger(__name__)
 logger.info("example.py")
 
-# Directory for output
-directory = "demo"
+results = []
+
+# Directories for output
+directory1 = "demo-images"
+directory2 = "demo-images-mount-bold"
 
 # assemble queries
 
+# assemble_query() takes two araguments, a query, and a output directory
 # returns tuple - (path, query_hash_prefix)
-query = assemble_query("sequence_data_type:site-image", directory)
-query2 = assemble_query("sequence_data_type:ont-minion", directory)
 
-#
-results = [query2, query]
+# assemble_query("type:gap-illumina-shortread sample_id:102.100.100/79638","gapdata")
+
+# put these two queries in the same directory
+results.append(assemble_query("sequence_data_type:image woomera", directory1))
+results.append(assemble_query("sequence_data_type:image camp mountain", directory1))
+
+# different directory for these results
+results.append(assemble_query("sequence_data_type:image mount bold", directory2))
+
+# download the results
 
 for r in results:
     download_query(r)
